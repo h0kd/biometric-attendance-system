b2d963440a7967f7e25681641d12c834c0cf1290
h0kd <pomillo11@gmail.com>
2023-11-14 09:57:15 -0300

    modified tables

--- ventaas/php/onBoardE.php
+++ ventaas/php/onBoardE.php
@@ -13,39 +13,44 @@
             </div>
         </div>
     </div>
-    <h1 id="h1Tabla">Tabla de Busqueda</h1>
-    <table border="1" class="tabla-personalizada">
-        <thead>
-            <tr>
-                <th>clase</th>
-                <th>Fecha y hora</th>
-                <th>Atrasos</th>
-            </tr>
-        </thead>
-        <tbody id="tablaBody">
-            
-        <?php
-        require 'dbcon.php';
-        $rut = $_GET['rut'];
-        if ($con->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-        }
-        $sql = "SELECT a.clase_id, a.fecha, a.atraso FROM asistencia a WHERE a.estudiante_rut = '$rut'";
-        $result = $con->query($sql);
-        if ($result->num_rows > 0) {
-            while ($row = $result->fetch_assoc()) {
-                $clase_id = $row['clase_id'];
-                $fecha = $row['fecha'];
-                $atraso = $row['atraso'];
-                echo "<tr><td>$clase_id</td><td>$fecha</td><td>$atraso</td></tr>";
-            }
-        } else {
-            echo "0 results";
-        }
-        $con->close();
-        ?>
-        </tbody>
-    </table>
+    <div class="container">
+        
+        <div class="table-container">
+            <h1 id="h1Tabla">Tabla de Busqueda</h1>
+            <table border="1" class="tabla-personalizada">
+                <thead>
+                    <tr>
+                        <th>clase</th>
+                        <th>Fecha y hora</th>
+                        <th>Atrasos</th>
+                    </tr>
+                </thead>
+                <tbody id="tablaBody">
+                    
+                <?php
+                require 'dbcon.php';
+                $rut = $_GET['rut'];
+                if ($con->connect_error) {
+                    die("Connection failed: " . $conn->connect_error);
+                }
+                $sql = "SELECT a.clase_id, a.fecha, a.atraso FROM asistencia a WHERE a.estudiante_rut = '$rut'";
+                $result = $con->query($sql);
+                if ($result->num_rows > 0) {
+                    while ($row = $result->fetch_assoc()) {
+                        $clase_id = $row['clase_id'];
+                        $fecha = $row['fecha'];
+                        $atraso = $row['atraso'];
+                        echo "<tr><td>$clase_id</td><td>$fecha</td><td>$atraso</td></tr>";
+                    }
+                } else {
+                    echo "0 results";
+                }
+                $con->close();
+                ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
     <br>
 
     <a href="excelE.php?rut=<?php echo $_GET['rut']; ?>" id="btnsTable2" class="btn btn-primary float-end">Generar informe</a>
--- ventaas/php/onBoardP.php
+++ ventaas/php/onBoardP.php
@@ -13,62 +13,67 @@
             </div>
         </div>
     </div>
-    <h1 id="h1Tabla">Tabla de Busqueda</h1>
-    <table border="1" class="tabla-personalizada">
-        <thead>
-            <tr>
-                <th>Nombre</th>
-                <th>Asistencias</th>
-                <th>Días totales</th>
-                <th>Porcentaje total</th>
-            </tr>
-        </thead>
-        <tbody id="tablaBody">
-            
-            <?php
-            require 'dbcon.php';
-            $rut2 = $_GET['rut'];
-                if ($con -> connect_error) {
-                    die("Conexion fallida:". $con -> connect_error);
-                }
-                $sql = "SELECT * from estudiante";
-                $result = $con -> query($sql);
-                if ($result -> num_rows > 0) {
-                    while ($estudiante = $result->fetch_assoc()) {
-                        $sql2 = "SELECT nombre from usuario where id = " . $estudiante["usuario_id"];
-                        $result2 = $con->query($sql2);
-                        $usuario = mysqli_fetch_array($result2)[0];
+    <div class="container">
+        
+        <div class="table-container">
+            <h1 id="h1Tabla">Tabla de Busqueda</h1>
+            <table border="1" class="tabla-personalizada">
+                <thead>
+                    <tr>
+                        <th>Nombre</th>
+                        <th>Asistencias</th>
+                        <th>Días totales</th>
+                        <th>Porcentaje total</th>
+                    </tr>
+                </thead>
+                <tbody id="tablaBody">
                     
-                        $sql3 = "SELECT count(*) from asistencia where estudiante_rut = " . $estudiante["rut"] . " and asistencia = 1";
-                        $result3 = $con->query($sql3);
-                        $asistencia = mysqli_fetch_array($result3)[0];
-                        
-                        $sql_rut = "SELECT rut FROM estudiante WHERE usuario_id = " . $estudiante["usuario_id"]; 
-                        $result_rut = $con->query($sql_rut);
-                        $rut = "";
-                        if ($result_rut->num_rows > 0) {
-                            $row_rut = $result_rut->fetch_assoc();
-                            $rut = $row_rut["rut"];
-                        } else {
-                            echo "No se encontró un rut para el estudiante con el nombre $nombre.";
+                    <?php
+                    require 'dbcon.php';
+                    $rut2 = $_GET['rut'];
+                        if ($con -> connect_error) {
+                            die("Conexion fallida:". $con -> connect_error);
                         }
-                    
-                        $sql4 = "SELECT count(*) from clase";
-                        $result4 = $con->query($sql4);
-                        $clase = mysqli_fetch_array($result4)[0];
-                    
-                        $porcentaje = round(($asistencia / $clase) * 100, 2);
-                        echo "<tr><td><a href='onBoardE.php?rut=$rut'abrirVentanaInformacion(\"$usuario\", $asistencia, $clase, $porcentaje)'>$usuario</a></td><td>$asistencia</td><td>$clase</td><td>$porcentaje%</td><td></td></tr>";
-                    }                    
-                    echo "</table";
-                } else {
-                    echo "0 resultados";
-                }
-                $con -> close();
-            ?>
+                        $sql = "SELECT * from estudiante";
+                        $result = $con -> query($sql);
+                        if ($result -> num_rows > 0) {
+                            while ($estudiante = $result->fetch_assoc()) {
+                                $sql2 = "SELECT nombre from usuario where id = " . $estudiante["usuario_id"];
+                                $result2 = $con->query($sql2);
+                                $usuario = mysqli_fetch_array($result2)[0];
+                            
+                                $sql3 = "SELECT count(*) from asistencia where estudiante_rut = " . $estudiante["rut"] . " and asistencia = 1";
+                                $result3 = $con->query($sql3);
+                                $asistencia = mysqli_fetch_array($result3)[0];
+                                
+                                $sql_rut = "SELECT rut FROM estudiante WHERE usuario_id = " . $estudiante["usuario_id"]; 
+                                $result_rut = $con->query($sql_rut);
+                                $rut = "";
+                                if ($result_rut->num_rows > 0) {
+                                    $row_rut = $result_rut->fetch_assoc();
+                                    $rut = $row_rut["rut"];
+                                } else {
+                                    echo "No se encontró un rut para el estudiante con el nombre $nombre.";
+                                }
+                            
+                                $sql4 = "SELECT count(*) from clase";
+                                $result4 = $con->query($sql4);
+                                $clase = mysqli_fetch_array($result4)[0];
+                            
+                                $porcentaje = round(($asistencia / $clase) * 100, 2);
+                                echo "<tr><td><a href='onBoardE.php?rut=$rut'abrirVentanaInformacion(\"$usuario\", $asistencia, $clase, $porcentaje)'>$usuario</a></td><td>$asistencia</td><td>$clase</td><td>$porcentaje%</td><td></td></tr>";
+                            }                    
+                            echo "</table";
+                        } else {
+                            echo "0 resultados";
+                        }
+                        $con -> close();
+                    ?>
 
-        </tbody>
-    </table>
+                </tbody>
+            </table>
+        </div>
+    </div>
     <br>
 
     <a href="excel.php" id="btnsTable2" class="btn btn-primary float-end">Generar informe</a>
