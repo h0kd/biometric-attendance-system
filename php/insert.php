d31534658b9e3b60fb16ca7f8bf5b66109a328ed
hokd <pomillo11@gmail.com>
2023-11-20 22:19:35 -0300

    footer on insert, formInsert bold

--- php/insert.php
+++ php/insert.php
@@ -46,8 +46,18 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     <title>Formulario de Inserción de Asistencia</title>
     <link rel="stylesheet" href="../css/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/9a45125b50.js" crossorigin="anonymous"></script>
 </head>
 <body>
+
+    <div class="containerBanner">
+        <div class="banner">
+            <div class="banner-text">
+                <a href="#"><img id="logoUsm" src="../assets/logoUsm.png"></a>
+            </div>
+        </div>
+    </div>
+
     <form action="" method="post" id="formInsert">
         <label for="estudiante_rut">RUT del Estudiante:</label><br>
         <input type="text" id="estudiante_rut" name="estudiante_rut" readonly><br><br>
@@ -59,7 +69,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         <input type="text" id="asistencia" name="asistencia"><br><br>
         <label for="atraso">Atraso (1 para atraso, 0 para puntualidad):</label><br>
         <input type="text" id="atraso" name="atraso"><br><br>
-        <input type="submit" value="Enviar" class="btn btn-success">
+        <input type="submit" value="Enviar" class="btn btn-success" id=btnsTable2>
         <script>
             document.getElementById('estudiante_rut').value = "<?php echo $rut; ?>";
         </script>
@@ -72,5 +82,36 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
             window.location.href = 'onBoardAdmin.php';
         });
     </script>
+
+    <!-- Footer -->
+    <footer class="pie-pagina">
+      <div class="grupo-1">
+        <div class="box">
+          <figure>
+            <a href="#">
+              <img src="../assets/deinacrida.png" alt="Logo del footer">
+            </a>
+          </figure>
+        </div>
+        <div class="box">
+          <h2>SOBRE NOSOTROS</h2>
+          <p>Texto de ejemplo con mucho ejemplo para que no quede vacio xdd</p>
+          <p>Texto de ejemplo numero 2 con mucho ejemplo con el anterior xdd</p>
+        </div>
+        <div class="box">
+          <h2>CONTACTO</h2>
+          <div class="red-social">
+            <a href="#" class="fa fa-facebook"></a>
+            <a href="#" class="fa fa-instagram"></a>
+            <a href="#" class="fa fa-twitter"></a>
+            <a href="#" class="fa fa-youtube"></a>
+          </div>
+        </div>
+      </div>
+      <div class="grupo-2">
+        <small>&copy; 2023 <b>Deinacrida</b> - Todos los Derechos Reservados.</small>
+      </div>
+    </footer>
+
 </body>
 </html>
